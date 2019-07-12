242d29a20e31eeffca25d8d50b41a6ff526c0692
upasanadeka <upasanadeka1999@gmail.com>
2019-07-12 17:54:01 +0530

    alignment

--- client/src/containers/Form/form.js
+++ client/src/containers/Form/form.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { Component} from 'react';
 import '../Form/style.css';
 import '../../../node_modules/font-awesome/css/font-awesome.min.css'; 
 
-export default function form() {
+export default class form extends Component{
+
+    // constructor(props){
+    //     super(props)
+    //     handleChange=this.handleChange.bind(this)
+    // }
+    render() {
     return (
         <div>
             
-                <div>
-                    <img className='dark_background' />
+            <div>
+                <img className='dark_background' />
+            </div>
+            <div  className = "login-box" >
+                <div className="headBox">
+                    <img className="imgStyle" src={require('../../images/logo.jpg')} />
+                    <div className="titleStyle">Ricechat</div>
                 </div>
-                <div  className = "login-box" >
-                    <div className="headBox">
-                        <img className="imgStyle" src={require('../../images/logo.jpg')} />
-                        <div className="titleStyle">Ricechat</div>
-                    </div>
 
-                    <div className="text_Login">Have an account? Login.</div>
-                    
-                    <div className="email_Password">
-                        <div>
-                            <i className="fa fa-user" aria-hidden="true"></i>
-                            <input type="text" value="" placeholder="Email/Username" />
-                        </div>
-                        <i class="fa fa-lock" aria-hidden="true"></i>
-                        <input type="password" value="" placeholder="Password" />
+                <div className="text_Login">Have an account? Login.</div>
+                
+                <div className="email_Password">
+                    <div>
+                        <i className="fa fa-user" aria-hidden="true"></i>
+                        <input type="text" placeholder="Email/Username" name="email" />
                     </div>
-                    <button type="submit" className="getting_started"> Get Started</button>
+                    <i class="fa fa-lock" aria-hidden="true"></i>
+                    <input type="password"  placeholder="Password" name="password" />
                 </div>
+                <button type="submit" className="getting_started">Get Started</button>
+            </div>
             
         </div>
 
     )
+  }
 }
